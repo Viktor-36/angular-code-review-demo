241731760f127edb830c29f13e4a92547fb1a55b
Kopcik, Viktor <viktor.kopcik@t-systems.com>
2024-07-01 17:57:04 +0200

    Added initial component for animals

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AnimalsListComponent} from './features/animals/components/animals-list/animals-list.component';
+import {HomeComponent} from './features/home/components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'animals',
+    component: AnimalsListComponent
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
