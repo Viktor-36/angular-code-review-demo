2e4551f031f4bf11dae41c67c9d323f005aace13
Kopcik, Viktor <viktor.kopcik@t-systems.com>
2024-07-04 10:07:38 +0200

    Updated BehaviorSubject in animals service

--- src/app/features/animals/services/animals.service.ts
+++ src/app/features/animals/services/animals.service.ts
@@ -2,17 +2,14 @@ import {Injectable} from '@angular/core';
 import {Animal} from '../models/animal';
 import {HttpClient} from '@angular/common/http';
 import {BehaviorSubject, map, Observable} from 'rxjs';
-import {AuthorizationService} from '../../../core/services/authorization.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnimalsService {
-  private _animals: Animal[] = [];
   private _animalsSubject: BehaviorSubject<Animal[]> = new BehaviorSubject<Animal[]>([]);
 
   constructor(
-    private readonly authorizationService: AuthorizationService,
     private readonly httpClient: HttpClient) {
     this.loadAnimals().subscribe();
   }
@@ -22,21 +19,24 @@ export class AnimalsService {
   }
 
   public addAnimal(animal: Animal): void {
-    this._animals.push(animal);
-    this._animalsSubject.next(this._animals);
+    const tempAnimals: Animal[] = this._animalsSubject.getValue();
+    tempAnimals.push(animal);
+
+    this._animalsSubject.next(tempAnimals);
   }
 
   public removeAnimal(animal: Animal): void {
-    let index = this._animals.findIndex((a) => a.name === animal.name);
-    this._animals.splice(index, 1);
-    this._animalsSubject.next(this._animals);
+    const tempAnimals: Animal[] = this._animalsSubject.getValue();
+
+    let index = tempAnimals.findIndex((a) => a.name === animal.name);
+    tempAnimals.splice(index, 1);
+    this._animalsSubject.next(tempAnimals);
   }
 
   private loadAnimals(): Observable<Animal[]> {
     return this.httpClient.get<Animal[]>('/animals.json').pipe(
       map((animals: Animal[]) => {
-        this._animals = animals;
-        this._animalsSubject.next(this._animals);
+        this._animalsSubject.next(animals);
         return animals;
       })
     )
